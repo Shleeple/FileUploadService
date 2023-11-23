6c1258e6fefc6f0b8e21505d62250400645f115c
Shleeple <juanjo.sada10@gmail.com>
2023-11-23 14:21:18 -0600

    Pushed changes from local repo to Git repository - 2023-11-23 14:21:18

--- UploadFiles/UploadService/models.py
+++ UploadFiles/UploadService/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Document(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    document_file = models.FileField(upload_to='documents/')
+    isd = models.CharField(max_length=255)
+    word_count = models.IntegerField()
+    document_type = models.CharField(max_length=255)
+    unique_id = models.CharField(max_length=255, unique=True)
+    
